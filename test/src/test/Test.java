91c6e30a35965deb47061406f1b81d9a1aff4cdd
OkaAryanata <OkaAryanata@192.168.160.1>
2016-04-17 23:32:40 +0700

    Merge origin/master
    Conflicts:
    	test/src/test/Anggota.java
    	test/src/test/AplikasiPerpus.java

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -16,9 +16,10 @@ public class Test {
      */
     public static void main(String[] args) {
         AplikasiPerpus p = new AplikasiPerpus();
-        //p.menuAwal();
+        p.menuAwal();
         //p.menuPetugas();
-        p.menuPinjam();
+        //p.menuPinjam();
+        //p.menuCariBukuA();
     }
     
 }
